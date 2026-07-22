4820d75eed478d240672c01dcbd1106857dc6e4b
is44csv <isaac.vilela15@gmail.com>
2026-07-22 19:36:15 -0300

    Add files via upload

---
+++ Aula-6/main.py
@@ -0,0 +1,71 @@
+
+
+
+# 1
+numero =  int(input('numero inteiro: '))
+quadrado =  numero ** 2
+print(quadrado)
+
+
+# 2
+
+
+nome = 'Beatriz'
+sobrenome = 'Alves'
+print(nome +  ' ' + sobrenome)
+print(nome, sobrenome)
+
+
+# 3
+
+
+n1  = int(input('numero inteiro:'))
+n2  = int(input('numero inteiro:'))
+print(str(n1), str(n2))
+
+
+# 4
+
+
+v = 'Python'
+print(v,10)
+
+
+# 5
+
+
+frase = 'Bom dia'
+nome  =  input('Nome: ')
+
+
+print(frase, nome)
+
+# +
+print('isso é um texto ' + ' ' + '10')
+# print(10 + 10)
+
+
+
+# ,
+print('isso é um texto ',  '10')
+
+
+
+# % 
+
+
+print('isso é um texto  %d'%(10))
+
+
+
+# .format()
+
+
+print('isso é um texto  {}'.format(10))
+
+
+
+# f-string
+
+
+print(f'isso é um texto  {10} ')
