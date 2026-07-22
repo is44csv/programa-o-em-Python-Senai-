6faaa540ee27acb35b0e9231c38eb10bf6f5e62e
is44csv <isaac.vilela15@gmail.com>
2026-07-21 21:36:11 -0300

    Add files via upload

---
+++ Aula-4/app.py
@@ -0,0 +1,29 @@
+1
+numero = 10
+resultado = numero 
+print(resultado)
+
+2
+print('Nome:')
+print('Sobrenome: ')
+
+3
+nome  =  input('Digite o nome : ')
+sobrenome  =  input('Digite o sobrenome : ')
+# concatenar  == juntar  
+resultado = nome + ' ' + sobrenome 
+print(resultado)
+
+4
+a = input ('digite o primeiro numero:')
+b = input ('digite o segundo numero:')
+input (a + ' '+ b)
+
+5
+p = 'python'
+print (p , 1)
+
+6
+a =  ('cristo vive')
+b =  ('pra sempre reinara')
+print (a + ' ' + b)
