125f563ada50582d2a9a981e56c2353509393b0e
is44csv <isaac.vilela15@gmail.com>
2026-07-23 19:44:14 -0300

    Add files via upload

--- Aula-6/main.py
+++ Aula-6/main.py
@@ -1,71 +1,21 @@
-
-
-
-# 1
-numero =  int(input('numero inteiro: '))
-quadrado =  numero ** 2
-print(quadrado)
-
-
-# 2
-
-
-nome = 'Beatriz'
-sobrenome = 'Alves'
-print(nome +  ' ' + sobrenome)
-print(nome, sobrenome)
-
-
-# 3
-
-
-n1  = int(input('numero inteiro:'))
-n2  = int(input('numero inteiro:'))
-print(str(n1), str(n2))
-
-
-# 4
-
-
-v = 'Python'
-print(v,10)
-
-
-# 5
-
-
-frase = 'Bom dia'
-nome  =  input('Nome: ')
-
-
-print(frase, nome)
-
-# +
-print('isso é um texto ' + ' ' + '10')
-# print(10 + 10)
-
-
-
-# ,
-print('isso é um texto ',  '10')
-
-
-
-# % 
-
-
-print('isso é um texto  %d'%(10))
-
-
-
-# .format()
-
-
-print('isso é um texto  {}'.format(10))
-
-
-
-# f-string
-
-
-print(f'isso é um texto  {10} ')
+# Calcule o módulo ( % ) de 17 por 4.
+print(17/4)
+# Multiplique 9 por 3 e, em seguida, eleve o resultado ao quadrado.
+print(9 * 3)
+# Calcule a raiz quadrada de 81.
+input('raiz quadradda de 81 é:') 
+print ('então 9 x 9 é:'), print(9 * 9)
+# Adicione 20 a 3 vezes 4.
+print(20 + (3*4))
+# Multiplique 15 por 2 e, em seguida, subtraia 7.
+print((15/2)-7)
+# Eleve 5 ao cubo.
+print(5*5*5)
+# Calcule a média de 17, 21 e 25.
+print((17 + 21 + 25) / 3)
+# Multiplique 11 por 2 e adicione 7.
+print((11 * 2) + 7)
+# Subtraia 15 de 3 vezes 8 e divida o resultado por 2.
+print(15 - (3*8) / 2)
+# Eleve 2 à potência de 10.
+print(2*2*2*2*2*2*2*2*2*2)
