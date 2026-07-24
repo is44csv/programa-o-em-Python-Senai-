3c00e9de512629c07481b33a9de1c8359c19f21b
is44csv <isaac.vilela15@gmail.com>
2026-07-23 21:33:40 -0300

    Add files via upload

---
+++ Aula-7/main.py
@@ -0,0 +1,34 @@
+# EXERCÍCIOS SOBRE LISTAS:
+# Exercício 0: Escreva um programa que use a função range() para gerar os números pares de 2 a 20 e, em seguida, imprima cada número.
+minha_lista = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+lista_vazia = []
+primeiro_elemento = minha_lista
+print(primeiro_elemento)
+# Exercício 1: Crie uma lista chamada numeros que contenha os números inteiros de 1 a 10 e imprima-a na tela.
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+lista_vazia = []
+primeiro_elemento = numeros
+print(primeiro_elemento)
+# Exercício 2: Acesse e imprima o terceiro elemento da lista numeros.
+print(numeros[3])
+# Exercício 3: Adicione o número 9 à lista numeros e imprima a lista atualizada.
+numeros = [1, 2, 3, 9, 5, 6, 7, 8, 9, 10]
+lista_vazia = []
+primeiro_elemento = numeros
+print(primeiro_elemento)
+# Exercício 4: Remova o número 5 da lista numeros e imprima a lista resultante.
+numeros = [1, 2, 3, 9, 6, 7, 8, 9, 10]
+lista_vazia = []
+primeiro_elemento = numeros
+print(primeiro_elemento)
+# Exercício 5: Crie uma lista chamada carros contendo três nomes de carros diferentes. Em seguida, concatene essa lista com a lista numeros e imprima o resultado.
+carros = ['civic', 'astra', 'celta']
+lista_vazia = []
+primeiro_elemento = carros
+numeros = [1, 2, 3, 9, 6, 7, 8, 9, 10]
+lista_vazia = []
+segundo_elemento = numeros
+print(primeiro_elemento, segundo_elemento)
+# Suba as atividade para o github ** 
+
+
